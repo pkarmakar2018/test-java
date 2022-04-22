a1e3643e03bc698cbe59ba0bf0084742134edbcb
Prodip Karmakar <prodipkar@gmail.com>
2022-04-23 00:08:23 +0530

    updated log message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world !");
         System.out.println("Learning Git");
-        System.out.println("changes in feature branch 3 - from feature");
+        System.out.println("changes in feature branch 31 - from feature");
     }
 }
